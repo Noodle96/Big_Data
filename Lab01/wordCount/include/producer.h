3968c72bcbc0f09e1b34bb75c1eb3802bd7cf1fa
Noodle96 <jorgealfredomarino2@gmail.com>
2025-05-09 16:41:09 -0500

    rm old files

--- Lab01/wordCount/include/producer.h
+++
@@ -1,6 +0,0 @@
-#ifndef PRODUCER_H
-#define PRODUCER_H
-
-void producer();
-
-#endif
--- test/test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-
-int main() {
-    std::string s;
-    std::cout << "Tamaño máximo de un string (en bytes): " << s.max_size() << std::endl; // aprox 4e18 Bytes => 4e12 MB
-    return 0;
-}
