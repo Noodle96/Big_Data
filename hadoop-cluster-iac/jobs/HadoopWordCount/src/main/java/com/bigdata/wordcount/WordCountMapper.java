63f650e78e4a3974a4e12db1ec2edc78ad863980
Noodle96 <jorgealfredomarino2@gmail.com>
2026-05-05 19:13:18 -0500

    Add Hadoop WordCount job implementation with Maven configuration and necessary files

---
+++ hadoop-cluster-iac/jobs/HadoopWordCount/src/main/java/com/bigdata/wordcount/WordCountMapper.java
@@ -0,0 +1,31 @@
+package com.bigdata.wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    private final static IntWritable ONE = new IntWritable(1);
+    private final Text word = new Text();
+
+    @Override
+    protected void map(
+            LongWritable key,
+            Text value,
+            Context context
+    ) throws IOException, InterruptedException {
+
+        String line = value.toString();
+        StringTokenizer tokenizer = new StringTokenizer(line);
+
+        while (tokenizer.hasMoreTokens()) {
+            word.set(tokenizer.nextToken());
+            context.write(word, ONE);
+        }
+    }
+}
